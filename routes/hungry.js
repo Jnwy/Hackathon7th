44900ec5058fb08d8c58590648afb629bf493112
Jhang Junwei <j.junwei@gmail.com>
2015-05-17 01:20:19 +0800

    hungry!

--- routes/hungry.js
+++ routes/hungry.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET material page. */
 router.get('/', function(req, res, next) {
-  res.render('material', { title: '巫砲小隊' });
+  res.render('hungry', { title: '肚子餓惹' });
 });
 
 module.exports = router;
