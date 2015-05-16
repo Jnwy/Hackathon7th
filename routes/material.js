7b3acf3609166c4c49e1d375087368c0bc52599e
Jhang Junwei <j.junwei@gmail.com>
2015-05-17 01:01:42 +0800

    都不寫QQ

--- routes/material.js
+++ routes/material.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET material page. */
 router.get('/', function(req, res, next) {
-  res.render('material', { title: 'material' });
+  res.render('material', { title: '巫砲小隊' });
 });
 
 module.exports = router;
